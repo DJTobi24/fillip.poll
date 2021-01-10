2f9154af66b58cd7222db6217dcfad20181cadf1
DJTobi24 <hagen.tobias@jelts.de>
2021-01-11 00:29:14 +0100

    Ömpf

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ include("config/database.inc.php"); // <- Datenbank einbinden
 
 $sql = "SELECT * FROM `poll` WHERE `Aktiv` = 1 LIMIT 0,1"; // SQL String. Limit ist 1
 $query = mysqli_query($conn, $sql); // Query ausführen
-$row = mysqli_fetch_row($query); // In Array packen
-
+while ( $row = mysqli_fetch_row($query)) // In Array packen
+{
 
 
 echo "<b>Frage: ".$row[1]."</b><br>";
@@ -14,8 +14,9 @@ echo "<input type='hidden' name='pollid' value='".$row[0]."' />";
 $sql = "SELECT * FROM `poll_answers` WHERE `pollid` = ".$row[0];
 $query = mysqli_query($conn, $sql);
 while($row = mysqli_fetch_row($query))
-{
-    echo "<input type='radio' name='answer' value='".$row[0]."' />".$row[1]."<br>";
+    {
+        echo "<input type='radio' name='answer' value='".$row[0]."' />".$row[1]."<br>";
+    }
 }
 ?>
 <input type='submit' value='Abstimmen!'>
