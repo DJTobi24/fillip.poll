068e70d88640fa8e618198479a3b748a49d42b0c
DJTobi24 <hagen.tobias@jelts.de>
2021-01-10 22:50:08 +0100

    Create

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+$sql = "SELECT * FROM `poll` ORDER BY datum";
+
+$umfragen = mysqli_query( $conn, $sql );
+if ( ! $db_erg )
+{
+    die('Ungültige Abfrage: ' . mysql_error());
+}
+$anzahl_eintraege = mysqli_num_rows($umfragen);
+echo "<p>Anzahl der Gästebuch-Einträge: $anzahl_eintraege </p>";
+?>
+
 <form method="post" action="config/vote.php">
 <?php
 include("config/database.inc.php"); // <- Datenbank einbinden
@@ -19,4 +31,10 @@ while($row = mysqli_fetch_row($query))
 ?>
 <input type='submit' value='Abstimmen!'>
 
+</form>
+<br>
+<br>
+<form method="post" action="config/create_vote.php">
+    <input type='radio' name='create' value="Test"><br>
+    <input type='submit' value='Umfrage Erstellen'>
 </form>
