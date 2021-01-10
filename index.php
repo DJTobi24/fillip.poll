cb78908451eed9bd88cfd28c4726bce2331c2fbc
DJTobi24 <hagen.tobias@jelts.de>
2021-01-10 21:58:00 +0100

    Vote

--- index.php
+++ index.php
@@ -6,15 +6,24 @@ include("config/database.inc.php"); // <- Datenbank einbinden
 $sql = "SELECT * FROM `poll` WHERE `Aktiv` = 1 LIMIT 0,1"; // SQL String. Limit ist 1
 $query = mysqli_query($conn, $sql); // Query ausführen
 $row = mysqli_fetch_row($query); // In Array packen
-echo $row[1]."<br>"; // row[1] ist im unserem fall die Frage
-echo "<input type='hidden' name='pollid' value='".$row[0]."' />"; //Wir speichern uns die Poll ID für spätere Verwendung
-
-$sql = "SELECT * FROM `poll_answers` WHERE `pollid` = ".$row[0]; // Abfrage der Antwort wo die ID des Polls die unserer Frage ist
-$query = mysqli_query($conn, $sql); // Query ausführen
-while($row = mysqli_fetch_row($query)) // So lange bis das ende erreicht ist
+$sql2 = "SELECT * FROM `poll_ip` WHERE `IP` LIKE '%".substr($_SERVER['REMOTE_ADDR'], 0, strpos($_SERVER['REMOTE_ADDR'], ".", 4))."%' AND `PollID` = ".$row[0].";"; //Hier wird nach der IP des Benutzers gesucht. Hierbei werden die zeichen gelöscht, die nach dem zweiten Punkt vorkommen (wir errinnern uns, dass die ersten zwei Stellen immer gleich bleiben).
+$query2 = mysqli_query($conn, $sql2); // Ausführen
+$voted = mysqli_fetch_row($query2); // In Array
+if($voted != "") //Wenn nichts gefunden wurde, gabs nichts. Aber wenn was gefunden wurde (!=), hat der User schon gevoted
 {
-    echo "<input type='radio' name='answer' value='".$row[0]."' />".$row[1]."<br>"; // Radiobutton mit Frage ausgeben
+echo "Du hast leider schon gevotet!";
+}
+else
+{
+echo "<b>".$row[1]."</b><br>";
+echo "<input type='hidden' name='pollid' value='".$row[0]."' />";
+$sql = "SELECT * FROM `poll_answers` WHERE `pollid` = ".$row[0];
+$query = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_row($query))
+{
+    echo "<input type='radio' name='answer' value='".$row[0]."' />".$row[1]."<br>";
 }
 ?>
 <input type='submit' value='Abstimmen!' />
+
 </form>
