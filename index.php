fff8e04b8560f4f1ce1f06762c944ec9633ee4ea
DJTobi24 <hagen.tobias@jelts.de>
2021-01-10 21:43:12 +0100

    $conn

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include("config/database.inc.php"); // Einbinden der Verbindung zur Datenbank
 $conn = OpenCon();
 echo "Connected Successfully";
 $sql = "SELECT * FROM `poll` WHERE `Aktiv` = 1 LIMIT 0,1"; // SQL String. Limit ist 1
-$query = mysqli_query($sql); // Query ausführen
+$query = mysqli_query($conn, $sql); // Query ausführen
 $row = mysqli_fetch_row($query); // In Array packen
 echo $row[1]."<br>"; // row[1] ist im unserem fall die Frage
 echo "<input type='hidden' name='pollid' value='".$row[0]."' />"; //Wir speichern uns die Poll ID für spätere Verwendung
