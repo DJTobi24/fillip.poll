994994c4c332285c55bcb7b9b5857845acda01e2
DJTobi24 <hagen.tobias@jelts.de>
2021-01-10 21:30:33 +0100

    pull

--- config/database.inc.php
+++ config/database.inc.php
@@ -1,15 +1,17 @@
 <?php
-$link = mysqli_connect("127.0.0.1", "poll", "25152515?", "poll");
-
-if (!$link) {
-    echo "Fehler: konnte nicht mit MySQL verbinden." . PHP_EOL;
-    echo "Debug-Fehlernummer: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debug-Fehlermeldung: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Erfolg: es wurde ordnungsgemäß mit MySQL verbunden! Die Datenbank \"datenbank\" ist toll." . PHP_EOL;
-echo "Host-Informationen: " . mysqli_get_host_info($link) . PHP_EOL;
-
-
-?>
+function OpenCon()
+ {
+ $dbhost = "127.0.0.1";
+ $dbuser = "poll";
+ $dbpass = "25152515?";
+ $db = "poll";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+   
