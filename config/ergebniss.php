767602d4156693f64777e2ca8d797a197dbe66b4
DJTobi24 <hagen.tobias@jelts.de>
2021-01-10 22:13:07 +0100

    $conn 2

--- config/ergebniss.php
+++ config/ergebniss.php
@@ -6,11 +6,11 @@ while( $row= mysqli_fetch_row($query)) //Für alle 5 wiederholen
 {
     echo "<b>Ergenisse der Umfrage:<br>".$row[1]." vom ".$row[2]."</b>:<br>"; //$row[1] ist die Frage, $row[2] das Datum
 $sql2 = "SELECT COUNT(`PollID`) FROM `poll_ip` WHERE `PollID` = ".$row[0];
-$query2 = mysqli_query($sql2);
+$query2 = mysqli_query($conn, $sql2);
 $row2 = mysqli_fetch_row($query2);
     $teilnehmer = $row2[0]; //Teilnehmer in andere Variable schreiben
     $sql3 = "SELECT * FROM `poll_answers` WHERE `PollID` = ".$row[0];
-    $query3= mysqli_query($sql3);
+    $query3= mysqli_query($conn, $sql3);
     while($row3 = mysqli_fetch_row($query3))
     {
         echo "<table>"; // Tabelle...
