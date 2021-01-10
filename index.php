d33b0b606af5ea488f70c2a9b2686121c491c579
DJTobi24 <hagen.tobias@jelts.de>
2021-01-10 18:36:21 +0100

    Test1

---
+++ index.php
@@ -0,0 +1,10 @@
+<form method="post" action="config/vote.php">
+<?php
+include("config/database.inc.php"); // Einbinden der Verbindung zur Datenbank
+$sql = "SELECT * FROM `poll` WHERE `Aktiv` = 1 LIMIT 0,1"; // SQL String. Limit ist 1
+$query = mysql_query($sql); // Query ausführen
+$row = mysql_fetch_row($query); // In Array packen
+echo $row[1]."<br>"; // row[1] ist im unserem fall die Frage
+echo "<input type='hidden' name='PollID' value='".$row[0]."' />"; //Wir speichern uns die Poll ID für spätere Verwendung
+?>
+</form>
