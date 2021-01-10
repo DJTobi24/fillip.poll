9c314b0165e85a6a4fa4a926855d52047cb869ed
DJTobi24 <hagen.tobias@jelts.de>
2021-01-10 23:45:00 +0100

    Test 4

--- erstellen.php
+++ erstellen.php
@@ -24,7 +24,7 @@
             $anzahl = mysqli_num_rows($umfragen);
 
 
-            $sql = "INSERT INTO poll(`ID`, `Fragen`, `Datum`, `Aktiv`)VALUES ('".$anzahl."', '".$_POST["name"]."', NOW())";
+            $sql = "INSERT INTO poll('ID', 'Fragen', 'Datum', 'Aktiv')VALUES ('".$anzahl."', '".$_POST["name"]."', NOW())";
 
             if (mysqli_query($conn, $sql)) {
                echo "Umfrage Erfolgreich erstellt";
