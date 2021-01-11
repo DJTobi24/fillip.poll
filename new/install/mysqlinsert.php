90c327057308885a6f50c4b95f71ae9d0fba813c
DJTobi24 <hagen.tobias@jelts.de>
2021-01-11 19:42:02 +0100

    Test

--- new/install/mysqlinsert.php
+++ new/install/mysqlinsert.php
@@ -1,24 +1,69 @@
 <?php
 
-include("db_connect.php");
+include('../config/dbdata.php');
+/* PDO connection start */
+$conn = new PDO('mysql:host=' . $DATENBANK_HOST . ';dbname=' . $DATENBANK_NAME . ';charset=utf8', $DATENBANK_BENUTZER, $DATENBANK_PASSWORT);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);         
+$conn->exec("SET CHARACTER SET utf8");     
+/* PDO connection end */
 
+// your config
+$filename = 'database.sql';
+
+$maxRuntime = 8; // less then your max script execution limit
+
+
+$deadline = time()+$maxRuntime; 
+$progressFilename = $filename.'_filepointer'; // tmp file for progress
+$errorFilename = $filename.'_error'; // tmp file for erro
+
+
+
+($fp = fopen($filename, 'r')) OR die('failed to open file:'.$filename);
+
+// check for previous error
+if( file_exists($errorFilename) ){
+	die('<pre> previous error: '.file_get_contents($errorFilename));
+}
+
+// activate automatic reload in browser
+echo '<html><head> <meta http-equiv="refresh" content="'.($maxRuntime+2).'"><pre>';
+
+// go to previous file position
+$filePosition = 0;
+if( file_exists($progressFilename) ){
+	$filePosition = file_get_contents($progressFilename);
+	fseek($fp, $filePosition);
+}
+
+$queryCount = 0;
 $query = '';
-$sqlScript = file('databae.sql');
-foreach ($sqlScript as $line)	{
-	
-	$startWith = substr(trim($line), 0 ,2);
-	$endWith = substr(trim($line), -1 ,1);
-	
-	if (empty($line) || $startWith == '--' || $startWith == '/*' || $startWith == '//') {
+while( $deadline>time() AND ($line=fgets($fp, 1024000)) ){
+	if(substr($line,0,2)=='--' OR trim($line)=='' ){
 		continue;
 	}
-		
-	$query = $query . $line;
-	if ($endWith == ';') {
-		mysqli_query($conn,$query) or die('<div class="error-response sql-import-response">Promlem mit dem Import: <b>' . $query. '</b></div>');
-		$query= '';		
+
+	$query .= $line;
+	if( substr(trim($query),-1)==';' ){
+
+		$igweze_prep= $conn->prepare($query);
+
+		if(!($igweze_prep->execute())){ 
+			$error = 'Error performing query \'<strong>' . $query . '\': ' . print_r($conn->errorInfo());
+			file_put_contents($errorFilename, $error."\n");
+			exit;
+		}
+		$query = '';
+		file_put_contents($progressFilename, ftell($fp)); // save the current file position for 
+		$queryCount++;
 	}
 }
-echo '<div class="success-response sql-import-response">SQL Datei Erfolgreich Importiert</div>';
+
+if( feof($fp) ){
+	echo 'dump successfully restored!';
+}else{
+	echo ftell($fp).'/'.filesize($filename).' '.(round(ftell($fp)/filesize($filename), 2)*100).'%'."\n";
+	echo $queryCount.' queries processed! please reload or wait for automatic browser refresh!';
+}
 
 ?>
