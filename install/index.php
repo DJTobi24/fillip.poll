2527c9034699fb0c661287f28ba71baab099e48e
DJTobi24 <hagen.tobias@jelts.de>
2021-01-11 13:19:47 +0100

    test

--- install/index.php
+++ install/index.php
@@ -6,3 +6,19 @@ if($php_version<5)
   $php_error="PHP version is $php_version - too old!";
 }
 ?>
+
+<?php
+// declare function
+function find_SQL_Version() {
+    $output = shell_exec('mysql -V');
+    preg_match('@[0-9]+\.[0-9]+\.[0-9]+@', $output, $version);
+    return @$version[0]?$version[0]:-1;
+  }
+   
+  $mysql_version=find_SQL_Version();
+  if($mysql_version<5)
+  {
+    if($mysql_version==-1) $mysql_error="MySQL version will be checked at the next step.";
+    else $mysql_error="MySQL version is $mysql_version. Version 5 or newer is required.";
+  }
+  ?>
