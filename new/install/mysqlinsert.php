63e8b37404e5d9b3afda643a18b4e70e998b3714
DJTobi24 <hagen.tobias@jelts.de>
2021-01-11 20:20:36 +0100

    Dsd

--- new/install/mysqlinsert.php
+++ new/install/mysqlinsert.php
@@ -22,19 +22,6 @@ try {
 		$db->exec($sql2);
 		print("Impotiere Demo Kontent in die Tabelle: umfrage.\n");
 
-	$sql3 ="CREATE TABLE IF NOT EXISTS `umfrage_antwort` (
-			`id` int(11) NOT NULL AUTO_INCREMENT,
-			`umfrage_id` int(11) NOT NULL,
-			`antworten` text NOT NULL,
-			`stimmen` int(11) NOT NULL DEFAULT '0',
-			PRIMARY KEY (`id`)
-			) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;;" ;
-		$db->exec($sql3);
-		print("Erstelle Tabelle: umfrage_antwort.\n");
-
-	$sql4 ="INSERT INTO `umfrage_antwort` (`id`, `umfrage_id`, `antworten`, `stimmen`) VALUES (1, 1, 'PHP', 0), (2, 1, 'Python', 0), (3, 1, 'C#', 0), (4, 1, 'Java', 0);" ;
-		$db->exec($sql4);
-		print("Impotiere Demo Kontent in die Tabelle: umfrage_antwort.\n");
 
 } catch(PDOException $e) {
     echo $e->getMessage();//Remove or change message in production code
