41ea8ca964a94d4692252311922c663348c8485c
DJTobi24 <hagen.tobias@jelts.de>
2021-01-11 09:52:36 +0100

    ds

--- demo3/index.php
+++ demo3/index.php
@@ -1,61 +1,21 @@
-<?php 
-include('config/header.php');
+<?php
+include ('config/Poll.php');
+$poll = new Poll();
+$pollData = $poll->getPoll();
 ?>
-<title>Voting System mit PHP und MySQL</title>
-<style>
-ul {
-    list-style-type: none;    
-	margin: 0;
-    padding: 0;
+<form action="" method="post" name="pollForm">
+<?php
+foreach($pollData as $poll){
+echo "<h3>".$poll['question']."</h3>";
+$pollOptions = explode("||||", $poll['options']);
+echo "<ul>";
+for( $i = 0; $i < count($pollOptions); $i++ ) {
+echo '<li><input type="radio" name="options" value="'.$i.'" > '.$pollOptions[$i].'</li>';
 }
-ul li {
-    margin-bottom:2px;
-    padding:2px;
-     
+echo "</ul>";
+echo '<input type="hidden" name="pollid" value="'.$poll['pollid'].'">';
+echo '<br><input type="submit" name="vote" class="btn btn-primary" value="Vote">';
+echo '<a href="results.php?pollID="'.$poll['pollid']."> View Results</a>";
 }
-a:hover, a:active, a:focus, a:visited {
-    text-decoration: none;
-} 
-</style>
-<?php include('config/container.php');?>
-<div class="container">
-	<h2>Voting System mit PHP und MySQL</h2>		
-	<?php
-	include ('config/Poll.php');        
-	$poll = new Poll();
-	$pollData = $poll->getPoll();	
-	if(isset($_POST['vote'])){
-		$pollVoteData = array(
-			'pollid' => $_POST['pollid'],
-			'pollOptions' => $_POST['options']
-		);
-		$isVoted = $poll->updateVote($pollVoteData);
-		if($isVoted){
-			setcookie($_POST['pollid'], 1, time()+60*60*24*365);			
-			$voteMessage = 'Your have voted successfully.';
-		} else {
-			$voteMessage = 'Your had already voted.';
-		}
-	}
-	?>	
-	<div class="poll-container">	
-		<?php echo !empty($voteMessage)?'<div class="alert alert-danger"><strong>Warning!</strong> '.$voteMessage.'</div>':''; ?>		
-		<form action="" method="post" name="pollFrm">	
-			<?php 
-			foreach($pollData as $poll){
-				echo "<h3>".$poll['question']."</h3>"; 				
-				$pollOptions = explode("||||", $poll['options']);
-				echo "<ul>";
-				for( $i = 0; $i < count($pollOptions); $i++ ) {
-					echo '<li><input type="radio" name="options" value="'.$i.'" > '.$pollOptions[$i].'</li>';
-				}
-				echo "</ul>";
-				echo '<input type="hidden" name="pollid" value="'.$poll['pollid'].'">';
-				echo '<br><input type="submit" name="vote" class="btn btn-primary" value="Vote">';
-				echo '<a href="results.php?pollID="'.$poll['pollid']."> View Results</a>";	
-			} 
-			?>			
-		</form>		
-	</div>		
-</div>
-<?php include('config/footer.php');?>
+?>
+</form>
--- demo3/results.php
+++ demo3/results.php
@@ -1,30 +1,20 @@
-
-<title>Demo</title>
-<style>
-
-</style>
-
-<div class="container">
-	<h2>Poll and Voting System with PHP, MySQL & jQuery</h2>	
-	<?php
-	include ('Poll.php');        
-	$poll = new Poll();
-	$pollData = $poll->getPoll();
-	foreach($pollData as $poll) {
-		echo "<h3>".$poll['question']."</h3>"; 		
-		$pollOptions = explode("||||", $poll['options']);
-		$votes = explode("||||", $poll['votes']);
-		for( $i = 0; $i<count($pollOptions); $i++ ) {
-			$votePercent = '0%';
-			if($votes[$i] && $poll['voters']) {
-				$votePercent = round(($votes[$i]/$poll['voters'])*100);
-				$votePercent = !empty($votePercent)?$votePercent.'%':'0%';	
-			}			
-			echo '<h5>'.$pollOptions[$i].'</h5>';
-			echo '<div class="progress">';
-			echo '<div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:'.$votePercent.'">'.$votePercent.'</div></div>';
-		}
-	} 
-	?>	
-	<a class="btn btn-default read-more" href="index.php">Back to Poll</a>	
-</div>
+<?php
+include ('config/Poll.php');
+$poll = new Poll();
+$pollData = $poll->getPoll();
+foreach($pollData as $poll) {
+echo "<h3>".$poll['question']."</h3>";
+$pollOptions = explode("||||", $poll['options']);
+$votes = explode("||||", $poll['votes']);
+for( $i = 0; $i<count($pollOptions); $i++ ) {
+$votePercent = '0%';
+if($votes[$i] && $poll['voters']) {
+$votePercent = round(($votes[$i]/$poll['voters'])*100);
+$votePercent = !empty($votePercent)?$votePercent.'%':'0%';
+}
+echo '<h5>'.$pollOptions[$i].'</h5>';
+echo '<div class="progress">';
+echo '<div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:'.$votePercent.'">'.$votePercent.'</div></div>';
+}
+}
+?>
