ee35ef193aee3ab3409482cc79ff8d0edec8ad85
DJTobi24 <hagen.tobias@jelts.de>
2021-01-11 20:03:37 +0100

    Test

--- new/install/mysqlinsert.php
+++ new/install/mysqlinsert.php
@@ -1,69 +1,18 @@
 <?php
+include('dbdata.php');
+try {
+     $db = new PDO('mysql:host=' . $DATENBANK_HOST . ';dbname=' . $DATENBANK_NAME . ';charset=utf8', $DATENBANK_BENUTZER, $DATENBANK_PASSWORT);
+     $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling
+     $sql ="CREATE TABLE IF NOT EXISTS `umfragen` (
+		`id` int(11) NOT NULL AUTO_INCREMENT,
+		`frage` text NOT NULL,
+		`besch` text NOT NULL,
+		PRIMARY KEY (`id`)
+	) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;" ;
+     $db->exec($sql);
+     print("Created Umfrage Table.\n");
 
-include('../config/dbdata.php');
-/* PDO connection start */
-$conn = new PDO('mysql:host=' . $DATENBANK_HOST . ';dbname=' . $DATENBANK_NAME . ';charset=utf8', $DATENBANK_BENUTZER, $DATENBANK_PASSWORT);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);         
-$conn->exec("SET CHARACTER SET utf8");     
-/* PDO connection end */
-
-// your config
-$filename = 'database.sql';
-
-$maxRuntime = 8; // less then your max script execution limit
-
-
-$deadline = time()+$maxRuntime; 
-$progressFilename = $filename.'_filepointer'; // tmp file for progress
-$errorFilename = $filename.'_error'; // tmp file for erro
-
-
-
-($fp = fopen($filename, 'r')) OR die('failed to open file:'.$filename);
-
-// check for previous error
-if( file_exists($errorFilename) ){
-	die('<pre> previous error: '.file_get_contents($errorFilename));
+} catch(PDOException $e) {
+    echo $e->getMessage();//Remove or change message in production code
 }
-
-// activate automatic reload in browser
-echo '<html><head> <meta http-equiv="refresh" content="'.($maxRuntime+2).'"><pre>';
-
-// go to previous file position
-$filePosition = 0;
-if( file_exists($progressFilename) ){
-	$filePosition = file_get_contents($progressFilename);
-	fseek($fp, $filePosition);
-}
-
-$queryCount = 0;
-$query = '';
-while( $deadline>time() AND ($line=fgets($fp, 1024000)) ){
-	if(substr($line,0,2)=='--' OR trim($line)=='' ){
-		continue;
-	}
-
-	$query .= $line;
-	if( substr(trim($query),-1)==';' ){
-
-		$igweze_prep= $conn->prepare($query);
-
-		if(!($igweze_prep->execute())){ 
-			$error = 'Error performing query \'<strong>' . $query . '\': ' . print_r($conn->errorInfo());
-			file_put_contents($errorFilename, $error."\n");
-			exit;
-		}
-		$query = '';
-		file_put_contents($progressFilename, ftell($fp)); // save the current file position for 
-		$queryCount++;
-	}
-}
-
-if( feof($fp) ){
-	echo 'dump successfully restored!';
-}else{
-	echo ftell($fp).'/'.filesize($filename).' '.(round(ftell($fp)/filesize($filename), 2)*100).'%'."\n";
-	echo $queryCount.' queries processed! please reload or wait for automatic browser refresh!';
-}
-
 ?>
