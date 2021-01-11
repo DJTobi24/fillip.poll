41b0cdda561562898d6357ce5c65c5de6fd95e94
DJTobi24 <hagen.tobias@jelts.de>
2021-01-11 09:45:08 +0100

    database

--- demo3/config/Poll.php
+++ demo3/config/Poll.php
@@ -1,8 +1,21 @@
 <?php
 class Poll{
-	
-	include("database.inc.php");
-
+    private $host  = '127.0.0.1';
+    private $user  = 'poll3';
+    private $password   = '25152515?';
+    private $database  = 'poll3';            
+    private $dbConnect = false;
+    private $pollTable = 'poll';
+    public function __construct(){
+        if(!$this->dbConnect){ 
+            $conn = new mysqli($this->host, $this->user, $this->password, $this->database);
+            if($conn->connect_error){
+                die("Failed to connect with MySQL: " . $conn->connect_error);
+            }else{
+                $this->dbConnect = $conn;
+            }
+        }
+    }
 	private function getData($sqlQuery) {
 		$result = mysqli_query($this->dbConnect, $sqlQuery);
 		if(!$result){
