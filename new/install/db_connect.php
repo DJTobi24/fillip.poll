112b121bf27fbab31a069617f169dec57d813428
DJTobi24 <hagen.tobias@jelts.de>
2021-01-11 19:35:36 +0100

    OMG

--- new/install/db_connect.php
+++ new/install/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 function OpenCon()
  {
-  include 'dbdata.php';
+  include '../config/dbdata.php';
 
  $conn = new mysqli($DATENBANK_HOST, $DATENBANK_BENUTZER, $DATENBANK_PASSWORT,$DATENBANK_NAME) or die("Verbindung Fehlgeschlagen: %s\n". $conn -> error);
  echo '<script>console.log("Consolen LOG: Datenbank Verbunden")</script>';
--- new/install/mysqlinsert.php
+++ new/install/mysqlinsert.php
@@ -1,11 +1,24 @@
 <?php
 
-
-
-$sql = file_get_contents('database.sql');
-
 include("db_connect.php");
 
-/* execute multi query */
-$mysqli->multi_query($sql);
+$query = '';
+$sqlScript = file('database.sql');
+foreach ($sqlScript as $line)	{
+	
+	$startWith = substr(trim($line), 0 ,2);
+	$endWith = substr(trim($line), -1 ,1);
+	
+	if (empty($line) || $startWith == '--' || $startWith == '/*' || $startWith == '//') {
+		continue;
+	}
+		
+	$query = $query . $line;
+	if ($endWith == ';') {
+		mysqli_query($conn,$query) or die('<div class="error-response sql-import-response">Promlem mit dem Import: <b>' . $query. '</b></div>');
+		$query= '';		
+	}
+}
+echo '<div class="success-response sql-import-response">SQL Datei Erfolgreich Importiert</div>';
+
 ?>
