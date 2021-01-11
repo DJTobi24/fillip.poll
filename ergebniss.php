921a98cd249c61fb3b7419c1375978522fd92e9d
DJTobi24 <hagen.tobias@jelts.de>
2021-01-11 11:33:53 +0100

    Final Fast

--- ergebniss.php
+++ ergebniss.php
@@ -1,6 +1,6 @@
 <?php
 include 'config/config.inc.php';
-// Connect to MySQL
+// Mit dem Mysql Server Verbinden
 $pdo = pdo_connect_mysql();
 // If the GET request "id" exists (poll id)...
 if (isset($_GET['id'])) {
