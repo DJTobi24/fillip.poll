99d45a093bd4709f3d9f54430b20417d7a998f14
DJTobi24 <hagen.tobias@jelts.de>
2021-01-11 20:22:00 +0100

    test

--- new/install/mysqlinsert.php
+++ new/install/mysqlinsert.php
@@ -3,7 +3,6 @@ include('../config/dbdata.php');
 try {
      $db = new PDO('mysql:host=' . $DATENBANK_HOST . ';dbname=' . $DATENBANK_NAME . ';charset=utf8', $DATENBANK_BENUTZER, $DATENBANK_PASSWORT);
 	 $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling
-
      $sql ="CREATE DATABASE IF NOT EXISTS `phpumfrage` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" ;
 		$db->exec($sql1;
 		print("Erstelle phpumfrage Datenbank, wen sie noch nicht existiert.\n");
@@ -15,13 +14,12 @@ try {
      besch text NOT NULL,
 	 PRIMARY KEY (id)
 	 )ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;;" ;
-     	$db->exec($sql1);
-	 	print("Erstelle Tabelle: umfragen.\n");
+     $db->exec($sql1);
+	 print("Created $table Table.\n");
 
      $sql2 ="INSERT INTO `umfragen` (`id`, `frage`, `besch`) VALUES (1, 'Was ist deine Lieblings Programmiersprache?', '');" ;
 		$db->exec($sql2);
-		print("Impotiere Demo Kontent in die Tabelle: umfrage.\n");
-
+		print("Created $table Table.\n");
 
 } catch(PDOException $e) {
     echo $e->getMessage();//Remove or change message in production code
